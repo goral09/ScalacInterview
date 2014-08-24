efa86a7a25973cf8b3e36ccb0cf30ed24fb585d9
Mateusz Gorski <goral09@gmail.com>
2014-08-24 15:23:44 +0200

    solve function left

--- src/Board.scala
+++ src/Board.scala
@@ -2,6 +2,21 @@ package scalac.interview
 
 import Figures._
 
+trait TilesGuarded {
+  /*
+    Checks if any of already placed <b>figures</b> is already guarding passed <b>pos</b>
+  */
+  val isAnyGuarding: List[(Figure, (Int, Int))] => Tuple2[Int, Int] => Boolean =
+    figures => pos => figures.exists { case (figure, place) => isGuarding(figure, place)(pos) }
+  /*
+    Checks if placing <b>figure</b> on <b>place</b> attacks any of <b>figures</b>
+  */
+  val isAttackingAny: (Figure, (Int, Int)) => List[(Figure, (Int, Int))] => Boolean =
+    (figure, place) => figures => figures.exists({case (_, pl) => isGuarding(figure, place)(pl) })
+    
+  private def isGuarding(figure: Figure, pl: (Int, Int))(place: (Int, Int)): Boolean = figure guards(pl, place)
+}
+
 case class Size(x: Int, y: Int)
 
 class Board(val figures: List[(Figure, (Int, Int))])(implicit boardSize: Size) extends TilesGuarded {
@@ -12,24 +27,16 @@ class Board(val figures: List[(Figure, (Int, Int))])(implicit boardSize: Size) e
     }
     
     def placeFigure(f: Figure, p: (Int, Int)) = Board((f, p) ::  figures)
+    
     //Returns first empty and unguarded tile on board
     def emptyTiles(figure: Figure) = (1 to boardSize.x).toList.flatMap(col => (1 to boardSize.y).toList.map(row => (col, row))).view.filter({
-        case (x,y) => !(isAnyGuarding(figures)((x,y)) && isAttackingAny(figure, (x,y))(figures))
+        case (x,y) => (!isAnyGuarding(figures)((x,y)) && !isAttackingAny(figure, (x,y))(figures))
     })
     
+    //def getFirstEmpty(figure: Figure) = emptyTiles.collectFirst({case field if!isAttackingAny(figure, field)(figures) => field })
 }
   
 object Board {
-  def apply(Board: List[(Figure, (Int, Int))])(implicit size: Size): Board =
+  def apply(Board: List[(Figure, (Int, Int))] = List.empty)(implicit size: Size): Board =
     new Board(Board.sortBy[(Int, Int)](_._2))
-}
-
-trait TilesGuarded {
-  val isAnyGuarding: List[(Figure, (Int, Int))] => Tuple2[Int, Int] => Boolean =
-    list => pos => list.forall { case (figure, place) => isGuarding(figure, place)(pos) }
-    
-  val isAttackingAny: (Figure, (Int, Int)) => List[(Figure, (Int, Int))] => Boolean =
-    (figure, place) => figures => figures.exists({case (_, pl) => isGuarding(figure, place)(pl) })
-    
-  private def isGuarding(figure: Figure, pl: (Int, Int))(place: (Int, Int)): Boolean = figure guards(pl, place)
 }
