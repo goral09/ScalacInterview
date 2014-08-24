6a0167b5e2118ac9a7da1932d3bce17102125e3c
Mateusz Gorski <goral09@gmail.com>
2014-08-24 15:23:27 +0200

    solve function left

--- src/Board.scala
+++ src/Board.scala
@@ -5,12 +5,18 @@ import Figures._
 case class Size(x: Int, y: Int)
 
 class Board(val figures: List[(Figure, (Int, Int))])(implicit boardSize: Size) extends TilesGuarded {
+    //tracing helper fction
+    def trace[A](msg: String, a: A): A = {
+      println(s"$msg $a")
+      a
+    }
+    
     def placeFigure(f: Figure, p: (Int, Int)) = Board((f, p) ::  figures)
     //Returns first empty and unguarded tile on board
-    def emptyTiles = (1 to boardSize.x).toList.flatMap(col => (1 to boardSize.y).toList.map(row => (col, row))).view.filter({
-        case (x,y) => !isAnyGuarding(figures)((x,y))
+    def emptyTiles(figure: Figure) = (1 to boardSize.x).toList.flatMap(col => (1 to boardSize.y).toList.map(row => (col, row))).view.filter({
+        case (x,y) => !(isAnyGuarding(figures)((x,y)) && isAttackingAny(figure, (x,y))(figures))
     })
-    def getFirstEmpty(figure: Figure) = emptyTiles.collectFirst({case field if!isAttackingAny(figure, field)(figures) => field })
+    
 }
   
 object Board {
@@ -19,11 +25,11 @@ object Board {
 }
 
 trait TilesGuarded {
-  //TODO got a feeling these two functions can be combined into one
   val isAnyGuarding: List[(Figure, (Int, Int))] => Tuple2[Int, Int] => Boolean =
     list => pos => list.forall { case (figure, place) => isGuarding(figure, place)(pos) }
     
   val isAttackingAny: (Figure, (Int, Int)) => List[(Figure, (Int, Int))] => Boolean =
     (figure, place) => figures => figures.exists({case (_, pl) => isGuarding(figure, place)(pl) })
+    
   private def isGuarding(figure: Figure, pl: (Int, Int))(place: (Int, Int)): Boolean = figure guards(pl, place)
 }
